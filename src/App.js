9c74bf49a1133ec1c08062a5ce6659b5f7db746d
geekolance <akkpuvvadafreelance@gmail.com>
2023-07-28 04:03:11 -0500

    Adding black buttons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { Secondary1 } from './components/Button/Button.stories';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Secondary1 />
     </div>
   );
 }
--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -11,14 +11,11 @@ export default {
   }
 }
 
+const Template = args => <Button {...args} />
+
 export const Primary1 = () => <Button variant='primary1'>Button</Button>
 export const Primary2 = () => <Button variant='primary2'>Button</Button>
 export const Primary3 = () => <Button variant='primary3'>Button</Button>
-export const Secondary1 = () => <Button variant='secondary1'>Button</Button>
-export const Secondary2 = () => <Button variant='secondary2'>Button</Button>
-export const Secondary3 = () => <Button variant='secondary3'>Button</Button>
-
-const Template = args => <Button {...args} />
 
 export const ButtonWithIconPrimary = Template.bind({})
 ButtonWithIconPrimary.args = {
@@ -38,6 +35,10 @@ ButtonWithIconPrimary3.args = {
   icon: figma,
 }
 
+export const Secondary1 = () => <Button variant='secondary1'>Button</Button>
+export const Secondary2 = () => <Button variant='secondary2'>Button</Button>
+export const Secondary3 = () => <Button variant='secondary3'>Button</Button>
+
 export const ButtonWithIconSecondary = Template.bind({})
 ButtonWithIconSecondary.args = {
   variant: 'secondary1',
@@ -54,4 +55,26 @@ export const ButtonWithIconSecondary3 = Template.bind({})
 ButtonWithIconSecondary3.args = {
   variant: 'secondary3',
   icon: figmaBlack,
+}
+
+export const Black1 = () => <Button variant='Black1'>Button</Button>
+export const Black2 = () => <Button variant='Black2'>Button</Button>
+export const Black3 = () => <Button variant='Black3'>Button</Button>
+
+export const ButtonWithIconBlack1 = Template.bind({})
+ButtonWithIconBlack1.args = {
+  variant: 'Black1',
+  icon: figma,
+}
+
+export const ButtonWithIconBlack2 = Template.bind({})
+ButtonWithIconBlack2.args = {
+  variant: 'Black2',
+  icon: figma,
+}
+
+export const ButtonWithIconBlack3 = Template.bind({})
+ButtonWithIconBlack3.args = {
+  variant: 'Black3',
+  icon: figma,
 }
