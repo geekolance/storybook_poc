6ed4843df20c552cae12e88a4f21b1ad532b69a3
geekolance <akkpuvvadafreelance@gmail.com>
2023-08-03 02:56:12 -0500

    Adding homepage nav icon, css and stroies

--- src/components/Hompagenav/Nav.js
+++ src/components/Hompagenav/Nav.js
@@ -2,10 +2,21 @@ import React from 'react'
 import './Nav.css'
 
 export const Nav = (props) => {
-  const { text = '0', url, label = 'label', ...rest } = props
+  const {
+    title,
+    icon,
+    label,
+    variant = 'primary',
+    children,
+    ...rest
+  } = props
+
   return (
     <button className={`homepage-nav ${variant}`} {...rest}>
-      {url && <img src={icon} className="button-icon"></img>}
+      {icon && <img src={icon} className="nav-icon"></img>}
+      {title && <div className='title'>{title}</div>}
+      {label && <div className='label'>{label}</div>}
+      {children}
     </button>
   )
 }
